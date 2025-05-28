7edd973c236f22bbe60955da52c41ea6d1e9017e
nanasi725 <Yu.mae4800@gmail.com>
2025-05-29 00:03:16 +0900

    マインスイーパーエム

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,13 @@
 import { useState } from 'react';
 import styles from './page.module.css';
 
-
-
+const hight = 9;
+const width = 9;
+const Bomcount = 10;
 
 //8方向
 const directions = [
-  [-1,-1],
+  [-1, -1],
   [-1, 0],
   [-1, 1],
   [0, -1],
@@ -18,38 +19,30 @@ const directions = [
   [1, 1],
 ];
 
-
-
-
-
-
-
+const board: number[][] = Array.from({ length: hight }, () =>
+  Array.from({ length: width }, () => 0),
+);
 
 export default function Home() {
-  const [setBom,bomMap] = useState<number[][]>([
-    [0,0,0,0],
-    [0,0,0,0],
-    [0,0,0,0],
-    [0,0,0,0],
+  const [board, setBoard] = useState<number[][]>([
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const [, ] = useState(0);
+  const [,] = useState(0);
 
-  const clickHandler = (x:number,y:number):void => {
-
-    const  = structuredClone();
-    [] += 1;
-    ();
-    (( + 1) % 14);
+  const clickHandler = (x: number, y: number): void => {
+    //const  = structuredClone(); [] += 1;(); (( + 1) % 14);
   };
-
-
 
   return (
     <div className={styles.container}>
-      <div className={styles.back}
-      >
-        {/* style={{ backgroundPosition: `${-30 * }px` }} */}
-      </div>
+      <div className={styles.board}>{/* style={{ backgroundPosition: `${-30 * }px` }} */}</div>
       <button onClick={clickHandler}>クリック</button>
     </div>
   );
